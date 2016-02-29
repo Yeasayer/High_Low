a0b2da3c88cbe29a14858ba96cc6cd1a233a3eda
Joseph Acevedo <joseph.acevedo.mail@gmail.com>
2016-02-29 14:22:37 -0600

    Put in a failsafe inCase a silly person passes a nonnumerical argument in the command line.

--- whose_number_is_it_anyway.php
+++ whose_number_is_it_anyway.php
@@ -1,6 +1,13 @@
 <?php
-
-$realNum = mt_rand($argv[1], $argv[2]);
+if ((is_numeric($argv[1])) && (is_numeric($argv[2]))){
+	$realNum = mt_rand($argv[1], $argv[2]);
+} else {
+	fwrite(STDOUT, "Put in a whole number for a range!\n");
+	$argv[1] = fgets(STDIN);
+	fwrite(STDOUT, "Put in another whole number for a range!\n");
+	$argv[2] = fgets(STDIN);
+	$realNum = mt_rand($argv[1], $argv[2]);
+}
 $guessCount = 0;
 fwrite(STDOUT, "Guess a number fool!\n");
 $guessNum = fgets(STDIN);
