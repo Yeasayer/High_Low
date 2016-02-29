6108d5fd800382dce418f26dcc0b36a95b8401f5
Joseph Acevedo <joseph.acevedo.mail@gmail.com>
2016-02-29 12:45:29 -0600

    Using mt_rand for number generaton!

--- whose_number_is_it_anyway.php
+++ whose_number_is_it_anyway.php
@@ -2,7 +2,7 @@
 
 fwrite(STDOUT, "Guess a number fool!\n");
 
-$realNum = rand(1, 100);
+$realNum = mt_rand(1, 100);
 $guessNum = fgets(STDIN);
 $guessCount = 0;
 echo "$realNum\n";
@@ -11,13 +11,11 @@ do {
 	if ($guessNum > $realNum){
 		$guessCount += 1;
 		fwrite(STDOUT, "Guess lower fool!\n");
-		fwrite(STDOUT, "$guessCount guesses.\n");
 		$guessNum = fgets(STDIN);
 		
 	} else {
 		$guessCount += 1;
 		fwrite(STDOUT, "Guess higher fool!\n");
-		fwrite(STDOUT, "$guessCount guesses.\n");
 		$guessNum = fgets(STDIN);
 	}
 } while ($guessNum != $realNum);
